2c9b642c69ba9add72448ff11ea62839d8b86132
selha <sae65@drexel.edu>
2025-09-28 09:04:10 -0400

    collaborate page

---
+++ src/app/collaborate/collaborate.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'collaborate',
+  imports: [RouterModule],
+  templateUrl: './collaborate.html',
+  styleUrl: './collaborate.css'
+})
+export class Collaborate {
+
+}
