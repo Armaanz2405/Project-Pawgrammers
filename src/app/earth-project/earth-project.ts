8d985337c24cc83521c3c0e672b69071df112a80
Armaan Zahid <az598@drexel.edu>
2025-09-28 09:51:36 -0400

    half done project description page

--- src/app/earth-project/earth-project.ts
+++ src/app/earth-project/earth-project.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-earth-project',
   imports: [RouterModule],
   templateUrl: './earth-project.html',
-  styleUrl: './earth-project.css'
+  styleUrls: ['./earth-project.css']
 })
 export class EarthProject implements OnInit, OnDestroy {
+  
+  private router = inject(Router);
 
   ngOnInit() {
     document.body.style.overflow = 'auto'; // allow scrolling
@@ -15,5 +17,10 @@ export class EarthProject implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     document.body.style.overflow = ''; // reset when leaving page
+  }
+
+  // Navigate to collaborate page
+  goToCollaborate() {
+    this.router.navigate(['/collaborate']);
   }
 }
