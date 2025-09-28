20409d00dc04ee99b1b9360907d4d6f40d39990d
christyTran <ctrann5152@gmail.com>
2025-09-28 00:58:15 -0400

    Project earth selection added minus animation

--- src/app/projects-earth/projects-earth.ts
+++ src/app/projects-earth/projects-earth.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects-earth',
-  imports: [RouterModule],
   templateUrl: './projects-earth.html',
-  styleUrl: './projects-earth.css'
+  styleUrls: ['./projects-earth.css']
 })
 export class ProjectsEarth {
+  orbitItems = [
+    { name: 'Satellite', src: 'Satellite.png' },
+    { name: 'Moon', src: 'Moon.png' },
+    { name: 'Spaceship', src: 'Spaceship.png' }
+  ];
 
+  currentIndex = 0;
+
+  constructor(private router: Router) {}
+
+  moveLeft() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.orbitItems.length) % this.orbitItems.length;
+  }
+
+  moveRight() {
+    this.currentIndex =
+      (this.currentIndex + 1) % this.orbitItems.length;
+  }
+
+  goToResults() {
+    this.router.navigate(['/result-earth']);
+  }
 }
