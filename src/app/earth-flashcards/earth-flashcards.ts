e6c4748102b4c172447c745c2f8ebe48610e3855
Armaan <armaanzahid05@gmail.com>
2025-09-28 01:14:25 -0400

    flashcards project template

--- src/app/earth-flashcards/earth-flashcards.ts
+++ src/app/earth-flashcards/earth-flashcards.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,6 +7,20 @@ import { RouterModule } from '@angular/router';
   templateUrl: './earth-flashcards.html',
   styleUrl: './earth-flashcards.css'
 })
-export class EarthFlashcards {
+export class EarthFlashcards implements OnInit, OnDestroy {
 
+  ngOnInit() {
+    document.body.style.overflow = 'auto'; // allow scrolling
+  }
+
+  ngOnDestroy() {
+    document.body.style.overflow = ''; // reset when leaving page
+  }
+
+  scrollTo(sectionId: string) {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
 }
